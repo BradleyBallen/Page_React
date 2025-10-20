6fa502eaceb23eef7064d12a98898bff3fff2dd3
Bradley <bradleyballen162004@gmail.com>
2025-10-20 13:27:47 -0500

    update to use api v1

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import apiV1 from "./api/v1";
+
 
 dotenv.config();
 
@@ -12,5 +14,6 @@ app.use(helmet());
 
 // Health Check
 app.get("/health", (_req, res) => res.json({ status: "OK" }));
+app.use("/api/v1", apiV1);
 
 export default app;
