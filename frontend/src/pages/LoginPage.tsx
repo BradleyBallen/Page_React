e7823c6b73dd40746f243ea29239d41946190f50
julian20murcia <105228128+julian20murcia@users.noreply.github.com>
2025-10-23 15:27:52 -0500

    the file loginoage.tsk on folder pages was edited

---
+++ frontend/src/pages/LoginPage.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 via-white to-blue-200">
+      <LoginForm />
+    </div>
+  );
+}
